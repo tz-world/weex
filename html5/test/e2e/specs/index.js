c63e4a1a4e89914f51a2bb2ead874c6349504fd4
MrRaindrop <tekkahs@gmail.com>
2016-06-21 11:16:48 +0800

    * [html5] fix ci 4

--- html5/test/e2e/specs/index.js
+++ html5/test/e2e/specs/index.js
@@ -1,6 +1,6 @@
 module.exports = {
   'index page': function (browser) {
-    browser.url('http://localhost:8080/index.html')
+    browser.url('http://localhost:8088/index.html')
     browser.expect.element('#weex').to.be.present
     browser.end()
   }
